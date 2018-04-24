0037b6ad0889c7cb4085dae400ab6654db3da6aa
Showwtime <jason.f.murphy@gmail.com>
2018-04-24 16:07:37 -0700

    added gulp, and eslint

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename')
+    browserSync = require('browser-sync')
+    eslint = require('gulp-eslint');
+
+gulp.task('lint', function(){
+    return gulp
+    .src(['./js/*.js', '!node_modiles/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()) 
+});
+
+gulp.task('scripts', gulp.series('lint', function(){
+    return gulp
+    .src('./js/*.js') //Take this file
+    .pipe(uglify()) //input into here, and uglify
+    .pipe(rename({ extname: '.min.js'})) // take uglified file and rename .min.js
+    .pipe(gulp.dest('./build/js')); //put uglified and renamed file and put in 'build' folder
+})  );
+
+gulp.task('browser-sync', function(done){
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    
+    gulp.watch(['build/js/*.min.js', 'style.css', 'index.html']).on('change', browserSync.reload);
+    done();
+});
+
+gulp.task('watch', function(done){ //watches file for changes, and automatically saves
+    gulp.watch('./js/*.js', gulp.series('scripts')) //watches
+    done(); //when finished running, tells command its done
+});
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log("hello world");
+
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "058576887ce44ee6a07aaed0fbfbf536"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+})
+    .done(function(result) {
+    console.log(result);
+})
+    .fail(function(err) {
+    throw err;
+});
+
+
+$("#my-select-menu").on("change", function() {
+    var selected = $(this).val();
+    if (selected !== "") {
+      console.log("The value you picked is: " + selected);
+    }
+
+
+
+
+});
