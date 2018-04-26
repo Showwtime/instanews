18b95ae55d7792a35f82c6aa95f9990b57d0f700
Showwtime <jason.f.murphy@gmail.com>
2018-04-26 15:23:59 -0700

    Grid set up, sized, made responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// console.log('hello world');
-
 var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
 url += '?' + $.param({
   'api-key': '058576887ce44ee6a07aaed0fbfbf536'
@@ -8,9 +6,7 @@ $.ajax({
   url: url,
   method: 'GET',
 })
-//     // .done(function(result) {
-//     // console.log(result);
-// })
+
     .fail(function(err) {
     throw err;
 });
