9926cf572055c0cf9a83d8cc6ef653ede4fc4243
Showwtime <jason.f.murphy@gmail.com>
2018-04-27 16:39:41 -0700

    attached NTY API to script file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
-var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+$( document ).ready(function() {
+  console.log( "ready!" );
+
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
-  'api-key': '058576887ce44ee6a07aaed0fbfbf536'
+  'api-key': "058576887ce44ee6a07aaed0fbfbf536"
 });
 $.ajax({
   url: url,
   method: 'GET',
-})
-
-    .fail(function(err) {
-    throw err;
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
 });
 
 
-$('#my-select-menu').on('change', function() {
-    var selected = $(this).val();
-    if (selected !== '') {
-      // console.log('The value you picked is: ' + selected);
-    }
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // End of Document Ready
