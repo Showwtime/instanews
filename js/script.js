8b70c573e04c60a02eb4c23f23d955d9fd70b675
Showwtime <jason.f.murphy@gmail.com>
2018-04-30 18:32:22 -0700

    inserted news grid, and js to fetch/post api

--- js/script.js
+++ js/script.js
@@ -1,50 +1,54 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(document).ready(function() {
+  $("#select-menu").on("change", function() {
+    $("#grid").empty();
 
-// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "058576887ce44ee6a07aaed0fbfbf536"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+    var selected = $(this).val();
+    console.log(selected);
 
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+    url +=
+      "?" +
+      $.param({
+        "api-key": "058576887ce44ee6a07aaed0fbfbf536"
+      });
 
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+      .done(function(data) {
 
+        var rawData = data.results;
+        console.log(rawData, 'before filter');
 
-
-
-
-
-
-
-
+        var filtered = rawData.filter(function(value){
+          return value.multimedia.length > 0;
 
 
+        })
+        .slice (0, 12);
 
+        /**
+         * jQuery loop, .each loops through array and grabs the URL, headline, and image.
+         */
+        $.each(filtered, function(key, value) {
+          var storyUrl = value.url;
+          var storyBlurb = value.abstract;
+          var storyImage = value.multimedia[4].url;
 
+          // setup our html template as a string and concatenate
+          var html = '<div class"wrapper" a href="'+ storyUrl + '"target="_blank"">';
+          html += '<div class="grid-item" style="background: url(' + storyImage + ')">';
+          html += '<p class="news-caption">' + storyBlurb + '</p>';
+          html += '</div>';
+          html += '</div>';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          $('#grid').append(html);
+         
+        });
+      })
+      .fail(function(err) {
+        throw err;
+      });
+  });
 }); // End of Document Ready
